82a7cae46e8586a0dd52cd2b6c51a3bab6e17b1c
Jelena Markovic <jelenam@stanford.edu>
2016-09-02 14:28:01 -0700

    read it

--- selection/sampling/randomized/multiple_views/multiple_views.py
+++ selection/sampling/randomized/multiple_views/multiple_views.py
@@ -7,12 +7,11 @@ class multiple_views(object):
 
         self.samplers = samplers
         self.data_construct_map = data_construct_map
-        self.data_recontruct_map = data_reconstruct_map
+        self.data_reconstruct_map = data_reconstruct_map
         self.nviews = len(samplers)
-        self.opt_length = 0
-        self.data_length = 0
-        self.opt_slice = []
-        self.data_slice = []
+        self.opt_length, self.data_length = 0, 0
+        self.opt_slice, self.data_slice = [], []
+
         for i in range(self.nviews):
             self.opt_slice.append(slice(self.opt_length, self.opt_length+samplers[i].opt_length))
             self.data_slice.append(slice(self.data_length, self.data_length+samplers[i].data_length))
@@ -39,11 +38,13 @@ class multiple_views(object):
 
     def gradient(self, data_state, opt_state):
         opt_grad = np.zeros(self.opt_length)
-        data_reconstruct = np.dot(self.data_recontruct_map, data_state)
+        _data_reconstruct = np.dot(self.data_reconstruct_map, data_state)
         _reconstruct_data_grad = np.zeros(self.data_length)
         for i in range(self.nviews):
             _reconstruct_data_grad[self.data_slice[i]], opt_grad[self.opt_slice[i]] =\
-                self.samplers[i].gradient(data_reconstruct[self.data_slice[i]], opt_state[self.opt_slice[i]])
+                self.samplers[i].gradient(_data_reconstruct[self.data_slice[i]], opt_state[self.opt_slice[i]])
 
         data_grad = np.dot(self.data_construct_map, _reconstruct_data_grad)
         return data_grad, opt_grad
+
+
