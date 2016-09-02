62e5de1e16e6d23e4b0d8ab5f95ec00491c0313a
Jelena Markovic <jelenam@stanford.edu>
2016-09-02 14:03:34 -0700

    added data gradient

--- selection/sampling/randomized/multiple_views/multiple_views.py
+++ selection/sampling/randomized/multiple_views/multiple_views.py
@@ -1,6 +1,4 @@
 import numpy as np
-from regreg.smooth.glm import glm as regreg_glm
-import regreg.api as rr
 
 
 class multiple_views(object):
@@ -21,30 +19,31 @@ class multiple_views(object):
             self.opt_length += samplers[i].opt_length
             self.data_length +=samplers[i].data_length
 
-        _init_data_state = np.zeros(self.data_length)
+        _init_reconstruct_data_state = np.zeros(self.data_length)
         self.init_opt_state = np.zeros(self.opt_length)
         for i in range(self.nviews):
             self.init_opt_state[self.opt_slice[i]] = samplers[i].init_opt_state
-            _init_data_state[self.data_slice[i]] = samplers[i].init_data_state
+            _init_reconstruct_data_state[self.data_slice[i]] = samplers[i].init_data_state
 
-        self.data_state = np.dot(data_construct_map, _init_data_state)
+        self.data_state = np.dot(data_construct_map, _init_reconstruct_data_state)
 
 
     def projection(self, data_state, opt_state):
-        data_reconstruct = np.dot(self.data_reconstruct_map, data_state)
+        _data_reconstruct = np.dot(self.data_reconstruct_map, data_state)
         new_opt_state = np.zeros_like(opt_state)
         for i in range(self.nviews):
             new_opt_state[self.opt_slice[i]] = \
-                self.samplers[i].projection(data_reconstruct[self.data_slice[i]], opt_state[self.opt_slice[i]])[1]
+                self.samplers[i].projection(_data_reconstruct[self.data_slice[i]], opt_state[self.opt_slice[i]])[1]
         return data_state.copy(), new_opt_state
 
 
     def gradient(self, data_state, opt_state):
-        data_grad = np.zeros(self.data_length)
         opt_grad = np.zeros(self.opt_length)
         data_reconstruct = np.dot(self.data_recontruct_map, data_state)
+        _reconstruct_data_grad = np.zeros(self.data_length)
         for i in range(self.nviews):
-            data_grad[self.data_slice[i]], opt_grad[self.opt_slice[i]] =\
+            _reconstruct_data_grad[self.data_slice[i]], opt_grad[self.opt_slice[i]] =\
                 self.samplers[i].gradient(data_reconstruct[self.data_slice[i]], opt_state[self.opt_slice[i]])
 
+        data_grad = np.dot(self.data_construct_map, _reconstruct_data_grad)
         return data_grad, opt_grad
