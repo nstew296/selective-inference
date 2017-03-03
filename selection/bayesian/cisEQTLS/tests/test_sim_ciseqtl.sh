5b656f03c70b7b772e4fe19ea68dce7c8607e887
Jason Zhu <junjie.zhu.jason@gmail.com>
2017-03-03 11:20:56 -0800

    printing all resutls

--- selection/bayesian/cisEQTLS/tests/test_sim_ciseqtl.sh
+++ selection/bayesian/cisEQTLS/tests/test_sim_ciseqtl.sh
@@ -13,4 +13,9 @@ source /home/jjzhu/source_code/cis_eqtl_pipeline/.env/bin/activate
 # python sim_ciseqtl.py evalSimes -o $DIR -s $YSEED
 DIR=/scratch/PI/sabatti/controlled_access_data/cisEQTLS_sim_exp2
 YSEED=3
-python sim_ciseqtl.py evalSimes -o $DIR -s $YSEED
+
+
+for YSEED in {1..10}
+do  
+    python sim_ciseqtl.py evalSimes -o $DIR -s $YSEED
+done
