8e24a7b9af60c1f891a5dab6cce72a001dd68596
snigdhagit <psnigdha@umich.edu>
2019-05-01 13:50:00 -0400

    ran mse tests

--- selection/multiple_splits/test_bias_across_splits.py
+++ selection/multiple_splits/test_bias_across_splits.py
@@ -63,10 +63,15 @@ def test_mse(n=200, p=1000, nval=200, alpha= 2., rho=0.70, s=10, beta_type=1, sn
 
     bias_tar_randomized = 0.
     bias_tar_split = 0.
+    bias_tar_debiased = 0.
     bias_randomized = 0.
-    mse_randomized = 0.
+    bias_debiased = 0.
     bias_split = 0.
+    mse_randomized = 0.
     mse_split = 0.
+    mse_debiased = 0.
+    bias_weight_debiased = 0.
+    mse_weight_debiased = 0.
 
     for i in range(nsim):
         X, y, _, _, Sigma, beta, sigma = sim_xy(n=n, p=p, nval=nval, alpha=alpha, rho=rho, s=s, beta_type=beta_type,
@@ -82,9 +87,13 @@ def test_mse(n=200, p=1000, nval=200, alpha= 2., rho=0.70, s=10, beta_type=1, sn
         #glm_LASSO_1se, lam_1se, glm_LASSO_min, lam_min = glmnet_lasso_cv(X, y)
 
         alpha_target_randomized = np.zeros(B)
+        alpha_target_debiased = np.zeros(B)
         alpha_target_split = np.zeros(B)
+
         sel_mle = np.zeros(B)
+        debiased_sel_mle = np.zeros(B)
         est_split = np.zeros(B)
+        weight_debiased = np.zeros(B)
         for j in range(B):
             lasso_sol = lasso.gaussian(X,
                                        y,
@@ -92,26 +101,49 @@ def test_mse(n=200, p=1000, nval=200, alpha= 2., rho=0.70, s=10, beta_type=1, sn
                                        randomizer_scale=np.sqrt(n) * randomizer_scale * sigma_)
             signs = lasso_sol.fit()
             nonzero = signs != 0
-            alpha_target_randomized[j] = np.linalg.pinv(X[:, nonzero]).dot(X.dot(beta))[0]
 
-            (observed_target,
-             cov_target,
-             cov_target_score,
-             alternatives) = selected_targets(lasso_sol.loglike,
-                                              lasso_sol._W,
-                                              nonzero,
-                                              dispersion=dispersion)
+            if nonzero[0] == 1:
+                alpha_target_randomized[j] = np.linalg.pinv(X[:, nonzero]).dot(X.dot(beta))[0]
+                (observed_target,
+                 cov_target,
+                 cov_target_score,
+                 alternatives) = selected_targets(lasso_sol.loglike,
+                                                  lasso_sol._W,
+                                                  nonzero,
+                                                  dispersion=dispersion)
 
-            observed_target_uni = (observed_target[0]).reshape((1,))
-            cov_target_uni = (np.diag(cov_target)[0]).reshape((1, 1))
-            cov_target_score_uni = cov_target_score[0, :].reshape((1, p))
+                observed_target_uni = (observed_target[0]).reshape((1,))
+                cov_target_uni = (np.diag(cov_target)[0]).reshape((1, 1))
+                cov_target_score_uni = cov_target_score[0, :].reshape((1, p))
 
-            mle, _, _, _, _, _, _, _ = lasso_sol.selective_MLE(observed_target_uni,
-                                                               cov_target_uni,
-                                                               cov_target_score_uni,
-                                                               alternatives)
+                mle, _, _, _, _, _, _, _ = lasso_sol.selective_MLE(observed_target_uni,
+                                                                   cov_target_uni,
+                                                                   cov_target_score_uni,
+                                                                   alternatives)
 
-            sel_mle[j] = mle
+                sel_mle[j] = mle
+
+                alpha_target_debiased = beta[0]
+                (debiased_observed_target,
+                 debiased_cov_target,
+                 debiased_cov_target_score,
+                 alternatives) = debiased_targets(lasso_sol.loglike,
+                                                  lasso_sol._W,
+                                                  nonzero,
+                                                  penalty=lasso_sol.penalty,
+                                                  dispersion=dispersion)
+
+                debiased_observed_target_uni = (debiased_observed_target[0]).reshape((1,))
+                debiased_cov_target_uni = (np.diag(debiased_cov_target)[0]).reshape((1, 1))
+                debiased_cov_target_score_uni = debiased_cov_target_score[0, :].reshape((1, p))
+
+                debiased_mle, var_mle, _, _, _, _, _, _ = lasso_sol.selective_MLE(debiased_observed_target_uni,
+                                                                                  debiased_cov_target_uni,
+                                                                                  debiased_cov_target_score_uni,
+                                                                                  alternatives)
+
+                debiased_sel_mle[j] = debiased_mle
+                weight_debiased[j] = 1./var_mle
 
             subsample_size = int(split_fraction * n)
             sel_idx = np.zeros(n, np.bool)
@@ -123,46 +155,78 @@ def test_mse(n=200, p=1000, nval=200, alpha= 2., rho=0.70, s=10, beta_type=1, sn
             y_sel = y[sel_idx]
             X_sel = X[sel_idx, :]
 
-            lam_theory_split = sigma_ * np.mean(np.fabs(np.dot(X_sel[:, 1:].T, np.random.standard_normal((subsample_size, 2000)))).max(0))
+            lam_theory_split = sigma_ * np.mean(
+                np.fabs(np.dot(X_sel[:, 1:].T, np.random.standard_normal((subsample_size, 2000)))).max(0))
             lasso_split = lasso_full.gaussian(X_sel, y_sel, np.append(0.001, np.ones(p - 1) * lam_theory_split))
             lasso_soln = lasso_split.fit()
             active_LASSO = (lasso_soln != 0)
-            alpha_target_split[j] = np.linalg.pinv(X_inf[:, active_LASSO]).dot(X_inf.dot(beta))[0]
-            est_split[j] = np.linalg.pinv(X_inf[:, active_LASSO]).dot(y_inf)[0]
 
-            print("mle ", mle, nonzero.sum(), active_LASSO.sum())
+            if active_LASSO[0] == 1:
+                alpha_target_split[j] = np.linalg.pinv(X_inf[:, active_LASSO]).dot(X_inf.dot(beta))[0]
+                est_split[j] = np.linalg.pinv(X_inf[:, active_LASSO]).dot(y_inf)[0]
+
+        alpha_target_randomized = alpha_target_randomized[alpha_target_randomized != 0]
+        alpha_target_debiased = alpha_target_debiased[alpha_target_debiased != 0]
+        sel_mle = sel_mle[sel_mle !=0]
+        debiased_sel_mle = debiased_sel_mle[debiased_sel_mle != 0]
+        indices = np.argsort(weight_debiased)
+        weights = np.arange(B)[indices] + 1.
+        print("check weights ", weight_debiased, weights/weights.sum())
+        weight_debiased_sel_mle = (weights/weights.sum()) * debiased_sel_mle
 
         avg_target_randomized = np.mean(alpha_target_randomized)
         bias_tar_randomized += (avg_target_randomized - alpha)
         bias_randomized += (np.mean(sel_mle) - alpha)
         mse_randomized += ((np.mean(sel_mle) - alpha) ** 2)
 
+        avg_target_debiased = np.mean(alpha_target_debiased)
+        bias_tar_debiased += (avg_target_debiased - alpha)
+        bias_debiased += (np.mean(debiased_sel_mle) - alpha)
+        mse_debiased += ((np.mean(debiased_sel_mle) - alpha) ** 2)
+
+        bias_weight_debiased += (weight_debiased_sel_mle.sum() - alpha)
+        mse_weight_debiased += ((weight_debiased_sel_mle.sum() - alpha) ** 2)
+
+        alpha_target_split = alpha_target_split[alpha_target_split != 0]
+        est_split = est_split[est_split !=0]
+
         avg_target_split = np.mean(alpha_target_split)
         bias_tar_split += (avg_target_split-alpha)
         bias_split += (np.mean(est_split) - alpha)
         mse_split += ((np.mean(est_split) - alpha) ** 2)
-        print("bias in target so far ", bias_tar_randomized/float(i+1.), bias_tar_split/float(i+1.))
-        print("bias in estimate so far ", bias_randomized / float(i + 1.), bias_split / float(i + 1.))
-        print("mse so far ", mse_randomized/ float(i + 1.), mse_split/ float(i + 1.))
+        print("bias in target so far ", bias_tar_randomized/float(i+1.), bias_tar_split/float(i+1.), bias_tar_debiased/float(i+1.))
+        print("bias in estimate so far ", bias_randomized / float(i + 1.), bias_split / float(i + 1.), bias_debiased/float(i+1.), bias_weight_debiased/float(i+1.))
+        print("mse so far ", mse_randomized/ float(i + 1.), mse_split/ float(i + 1.), mse_debiased/float(i+1.), mse_weight_debiased/float(i+1.))
 
         print("iteration completed ", i)
     bias_tar_randomized/=float(nsim)
     bias_tar_split/=float(nsim)
+    bias_tar_debiased /=float(nsim)
     bias_randomized/=float(nsim)
     mse_randomized/=float(nsim)
+    bias_debiased/=float(nsim)
+    mse_debiased/=float(nsim)
     bias_split/=float(nsim)
     mse_split/=float(nsim)
+    bias_weight_debiased/=float(nsim)
+    mse_weight_debiased/=float(nsim)
+
     return np.vstack((bias_tar_randomized,
                       bias_tar_split,
+                      bias_tar_debiased,
                       bias_randomized,
                       bias_split,
+                      bias_debiased,
+                      bias_weight_debiased,
                       mse_randomized,
-                      mse_split))
+                      mse_split,
+                      mse_debiased,
+                      mse_weight_debiased))
 
 #test_mse(n=200, p=1000, nval=200, alpha= 2., rho=0.35, s=10, beta_type=0, snr=0.71, randomizer_scale=1., split_fraction=0.67, nsim=200, B=18)
 
 def output_file(n=200, p=1000, nval=200, alpha= 2., rho=0.35, s=10, beta_type=0, snr=0.71,
-                randomizer_scale=1., split_fraction=0.67, nsim=200, Bval=np.array([1,2,3,5,10,20,25]),
+                randomizer_scale=1., split_fraction=0.67, nsim=10, Bval=np.array([1,2,3,5,10,20,25]),
                 outpath= None):
 
     df_mse = pd.DataFrame()
@@ -180,10 +244,10 @@ def output_file(n=200, p=1000, nval=200, alpha= 2., rho=0.35, s=10, beta_type=0,
                           nsim=nsim,
                           B=B_agg)
 
-        df_mse_B = pd.DataFrame(data=output.reshape((1, 6)),
-                                columns=['bias_tar_randomized', 'bias_tar_split',
-                                         'bias_randomized', 'bias_split',
-                                         'mse_randomized', 'mse_split'])
+        df_mse_B = pd.DataFrame(data=output.reshape((1, 11)),
+                                columns=['bias_tar_randomized', 'bias_tar_split', 'bias_tar_debiased',
+                                         'bias_randomized', 'bias_split', 'bias_debiased', 'bias_weight_debiased',
+                                         'mse_randomized', 'mse_split', 'mse_debiased', 'mse_weight_debiased'])
 
         df_mse = df_mse.append(df_mse_B, ignore_index=True)
 
@@ -199,8 +263,8 @@ def output_file(n=200, p=1000, nval=200, alpha= 2., rho=0.35, s=10, beta_type=0,
     if outpath is None:
         outpath = os.path.dirname(__file__)
 
-    outfile_inf_html = os.path.join(outpath, "dims_" + str(n) + "_" + str(p) + "_mse_compare_N_" + str(beta_type) +  "_rho_" + str(rho) + ".html")
-    outfile_inf_csv = os.path.join(outpath,"dims_" + str(n) + "_" + str(p) + "_mse_compare_N_" + str(beta_type) + "_rho_" + str(rho) + ".csv")
+    outfile_inf_html = os.path.join(outpath, "dims_" + str(n) + "_" + str(p) + "_mse_compare_Dnormal_" + str(beta_type) +  "_rho_" + str(rho) + ".html")
+    outfile_inf_csv = os.path.join(outpath,"dims_" + str(n) + "_" + str(p) + "_mse_compare_Dnormal_" + str(beta_type) + "_rho_" + str(rho) + ".csv")
 
     df_mse.to_csv(outfile_inf_csv, index=False)
     df_mse.to_html(outfile_inf_html)
@@ -209,7 +273,7 @@ output_file(n=200, p=1000, nval=200, alpha= 2., rho=0.35, s=10, beta_type=0, snr
             randomizer_scale=1.,
             split_fraction=0.67,
             nsim=200,
-            Bval=np.array([1, 2, 3, 5, 10, 20, 25, 50, 100]),
+            Bval= np.array([1, 2, 3, 5, 10, 20, 25, 50]),
             outpath= None)
 
 
