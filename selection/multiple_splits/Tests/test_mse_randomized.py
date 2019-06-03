1bbe9ba2006cbc9872d55b637bd786aa82cd33b2
snigdhagit <psnigdha@umich.edu>
2019-06-03 12:32:07 -0400

    commit changes for now

--- selection/multiple_splits/Tests/test_mse_randomized.py
+++ selection/multiple_splits/Tests/test_mse_randomized.py
@@ -27,12 +27,16 @@ def test_mse(n=200, p=1000, nval=200, alpha=2., rho=0.70, s=10, beta_type=1, snr
         X, y, _, _, Sigma, beta, sigma = sim_xy(n=n, p=p, nval=nval, alpha=alpha, rho=rho, s=s, beta_type=beta_type,
                                                 snr=snr)
         X -= X.mean(0)[None, :]
-        X /= (X.std(0)[None, :] * np.sqrt(n / (n - 1.)))
         y = y - y.mean()
+        OLS_weights = np.fabs(X.T.dot(y))
 
+
+        X /= (X.std(0)[None, :] * np.sqrt(n / (n - 1.)))
         dispersion = None
         sigma_ = np.std(y)
-        lam_theory = sigma_ * 0.85 *  np.mean(np.fabs(np.dot(X[:, 1:].T, np.random.standard_normal((n, 2000)))).max(0))
+        lam_theory = sigma_ * 0.60 *  np.mean(np.fabs(np.dot(X[:, 1:].T, np.random.standard_normal((n, 2000)))).max(0))
+        #HOLP = np.abs(X.T.dot(np.linalg.inv(X.dot(X.T)+ 0.5)).dot(y))
+
 
         alpha_target_randomized = np.zeros(B)
         sel_mle = np.zeros(B)
@@ -43,12 +47,15 @@ def test_mse(n=200, p=1000, nval=200, alpha=2., rho=0.70, s=10, beta_type=1, snr
         for j in range(B):
             lasso_sol = lasso.gaussian(X,
                                        y,
-                                       feature_weights=np.append(0.001, np.ones(p - 1) * lam_theory),
+                                       #feature_weights=np.append(0.001, np.ones(p-1)*lam_theory),
+                                       feature_weights=np.append(0.001, (n*n)/OLS_weights[1:]),
                                        randomizer_scale=np.sqrt(n) * randomizer_scale * sigma_)
             signs = lasso_sol.fit()
             nonzero = signs != 0
+            #print("check ", np.diag(X.T.dot(X)), nonzero.sum(), (np.sqrt(n)*n)/OLS_weights[1:], lam_theory)
+            print("check ", nonzero.sum())
 
-            if nonzero[0] == 1 and nonzero.sum()<30:
+            if nonzero[0] == 1:
                 alpha_target_randomized[j] = np.linalg.pinv(X[:, nonzero]).dot(X.dot(beta))[0]
                 (observed_target,
                  cov_target,
@@ -79,14 +86,17 @@ def test_mse(n=200, p=1000, nval=200, alpha=2., rho=0.70, s=10, beta_type=1, snr
             y_sel = y[sel_idx]
             X_sel = X[sel_idx, :]
 
-            lam_theory_split = sigma_ * 0.85* np.mean(
-                np.fabs(np.dot(X_sel[:, 1:].T, np.random.standard_normal((subsample_size, 2000)))).max(0))
-            lasso_split = lasso_full.gaussian(X_sel, y_sel, np.append(0.001, np.ones(p - 1) * lam_theory_split))
+            HOLP_split = np.abs(X_sel.T.dot(np.linalg.inv(X_sel.dot(X_sel.T)+0.5)).dot(y_sel))
+
+            lam_theory_split = sigma_ * 0.60 * np.mean(np.fabs(np.dot(X_sel[:, 1:].T, np.random.standard_normal((subsample_size, 2000)))).max(0))
+            lasso_split = lasso_full.gaussian(X_sel, y_sel, np.append(0.001, np.ones(p - 1) * split_fraction * (n*n)/OLS_weights[1:]))
+            #lasso_split = lasso_full.gaussian(X_sel, y_sel, np.append(0.001, 1./HOLP_split[1:]))
             lasso_soln = lasso_split.fit()
             active_LASSO = (lasso_soln != 0)
             nactive_LASSO = active_LASSO.sum()
+            print("check SPLIT", nactive_LASSO)
 
-            if active_LASSO[0] == 1 and nactive_LASSO<30:
+            if active_LASSO[0] == 1:
                 alpha_target_split[j] = np.linalg.pinv(X_inf[:, active_LASSO]).dot(X_inf.dot(beta))[0]
                 est_split[j] = np.linalg.pinv(X_inf[:, active_LASSO]).dot(y_inf)[0]
 
@@ -185,8 +195,8 @@ def output_file(n=200, p=1000, nval=200, alpha= 2., rho=0.35, s=10, beta_type=0,
 output_file(n=100, p=500, nval=100, alpha= 2., rho=0.35, s=5, beta_type=1, snr=0.71,
             randomizer_scale=1.,
             split_fraction=0.67,
-            nsim= 1000,
-            Bval= np.array([1, 2, 3, 5, 10, 20, 25, 50]),
+            nsim= 200,
+            Bval= np.array([1, 2, 3, 5, 10, 15]),
             outpath= None)
 
 
