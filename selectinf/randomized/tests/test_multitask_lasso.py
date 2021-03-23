2d6c528a0179d159a1354445ce2fde77303e6f78
Natasha Stewart <nstew@umich.edu>
2021-03-23 15:20:12 -0400

    updates hetero test imports:

--- selectinf/randomized/tests/test_multitask_lasso.py
+++ selectinf/randomized/tests/test_multitask_lasso.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib
-#matplotlib.use('TkAgg')
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 from scipy.stats import norm as ndist
 from scipy.stats import t as tdist
