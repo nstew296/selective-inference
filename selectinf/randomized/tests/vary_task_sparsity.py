5dd591b3fe588d603d8c0760134928783e34d61c
nstew <nstew@umich.edu>
2021-12-10 18:31:03 -0500

    updates to simulations

--- selectinf/randomized/tests/vary_task_sparsity.py
+++ selectinf/randomized/tests/vary_task_sparsity.py
@@ -7,13 +7,13 @@ import pandas as pd
 from selectinf.randomized.tests.test_multitask_lasso import test_coverage
 
 k=5
-p=250
+p=100
 #global_sparsity = 0.95
 task_sparsity = 0.4
 
 length_path = 15
 lambdamin = 0
-lambdamax = 5.5
+lambdamax = 4.0
 #weights = np.arange(np.log(lambdamin), np.log(lambdamax), (np.log(lambdamax) - np.log(lambdamin)) / (length_path))
 #feature_weight_list = np.exp(weights)
 feature_weight_list = np.arange(lambdamin, lambdamax,(lambdamax - lambdamin) / (length_path))
@@ -23,7 +23,7 @@ df = pd.DataFrame(columns=['Task Sparsity', 'Method', 'Coverage', 'Length'])
 
 
 #sparsity_list = [0.0,0.2,0.4,0.6]
-sparsity_list = [0.85,0.90,0.95,0.99]
+sparsity_list = [0.80,0.85,0.9,0.95]
 n_list = [100,100,100,100]
 ##n_list = [5,5,5,20,20]
 coverage_by_ts = {j: [[], [], [], [], [], [], []] for j in range(len(sparsity_list))}
@@ -78,7 +78,7 @@ for j in range(len(sparsity_list)):
 
     for i in range(len(feature_weight_list)):
         print((i,j),"(i,j)")
-        sims = test_coverage(feature_weight_list[i],[2.5,5.0],sparsity_list[j],nsim=n_list[j])
+        sims = test_coverage(feature_weight_list[i],[2.5,5.0],p,task_sparsity,sparsity_list[j],nsim=n_list[j])
         selective_coverage.append(sims[3])
         selective_coverage2.append(sims[4])
         naive_coverage.append(sims[5])
@@ -243,7 +243,7 @@ set_box_color(fourth, '#238443','solid')
 set_box_color(fifth, '#31a354','--')
 set_box_color(sixth, '#fd8d3c','solid')
 set_box_color(seventh,'#feb24c','--')
-plt.xticks(range(1, (length) * 3 + 1, 3), [round(num, 1) for num in sparsity_list])
+plt.xticks(range(1, (length) * 3 + 1, 3), [round(num, 2) for num in sparsity_list])
 plt.xlim(-1, (length - 1) * 3 + 3)
 plt.plot([], c='#D7191C', label='Naive',linewidth=2.5)
 plt.plot([], c='#2b8cbe', label='Randomized Multi-Task Lasso 0.7',linewidth=2.5)
@@ -269,7 +269,7 @@ set_box_color(fourth, '#238443','solid')
 set_box_color(fifth, '#31a354','--')
 set_box_color(sixth, '#fd8d3c','solid')
 set_box_color(seventh,'#feb24c','--')
-plt.xticks(range(1, (length) * 3 + 1, 3), [round(num, 1) for num in sparsity_list])
+plt.xticks(range(1, (length) * 3 + 1, 3), [round(num, 2) for num in sparsity_list])
 plt.xlim(-1, (length - 1) * 3 + 3)
 plt.tight_layout()
 plt.ylabel('Interval Length',fontsize=12)
@@ -287,7 +287,7 @@ set_box_color(fourth, '#238443','solid')
 set_box_color(fifth, '#31a354','--')
 set_box_color(sixth, '#fd8d3c','solid')
 set_box_color(seventh,'#feb24c','--')
-plt.xticks(range(1, (length) * 3 + 1, 3), [round(num, 1) for num in sparsity_list])
+plt.xticks(range(1, (length) * 3 + 1, 3), [round(num, 2) for num in sparsity_list])
 plt.xlim(-1, (length - 1) * 3 + 3)
 plt.tight_layout()
 plt.ylabel('f1 per Simulation',fontsize=12)
@@ -296,7 +296,6 @@ plt.ylabel('f1 per Simulation',fontsize=12)
 ax1.set_title("Coverage", y = 1.01)
 ax2.set_title("Length", y = 1.01)
 ax3.set_title("Accuracy", y = 1.01)
-fig.suptitle("Regression Dimension p=250",fontsize=14)
 
 
 def common_format(ax):
@@ -311,10 +310,10 @@ common_format(ax3)
 
 # add target coverage on the first plot
 ax1.axhline(y=0.9, color='k', linestyle='--', linewidth=2)
-
+fig.suptitle("Task Sparsity 40 Percent, Regression Dimension p=100",fontsize=16)
 plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
 ax1.legend(loc='lower left', bbox_to_anchor=(0.6, -0.45),fontsize=14,ncol=3)
-plt.savefig('vary_global_sparsity_p250.png', bbox_inches='tight')
+plt.savefig('vary_global_sparsity_p100.png', bbox_inches='tight')
 
 
 #fig = plt.figure(figsize=(25, 10))
