a95fd0981a70d9a07a8e6d3371c6619006964c7a
nstew <nstew@umich.edu>
2021-12-13 16:12:43 -0500

    updates to simulation plots

--- selectinf/randomized/tests/vary_task_sparsity.py
+++ selectinf/randomized/tests/vary_task_sparsity.py
@@ -8,8 +8,8 @@ from selectinf.randomized.tests.test_multitask_lasso import test_coverage
 
 k=5
 p=100
-#global_sparsity = 0.95
-task_sparsity = 0.4
+global_sparsity = 0.95
+#task_sparsity = 0.4
 
 length_path = 15
 lambdamin = 0
@@ -22,8 +22,8 @@ print(feature_weight_list)
 df = pd.DataFrame(columns=['Task Sparsity', 'Method', 'Coverage', 'Length'])
 
 
-#sparsity_list = [0.0,0.2,0.4,0.6]
-sparsity_list = [0.80,0.85,0.9,0.95]
+sparsity_list = [0.0,0.2,0.4,0.6]
+#sparsity_list = [0.80,0.85,0.9,0.95]
 n_list = [100,100,100,100]
 ##n_list = [5,5,5,20,20]
 coverage_by_ts = {j: [[], [], [], [], [], [], []] for j in range(len(sparsity_list))}
@@ -32,8 +32,8 @@ f1_by_ts = {j: [[], [], [], [], [], []] for j in range(len(sparsity_list))}
 
 
 for j in range(len(sparsity_list)):
-    #positive = (1.-global_sparsity)*(1.-sparsity_list[j])*k*p
-    positive = (1.-task_sparsity)*(1.-sparsity_list[j])*k*p
+    positive = (1.-global_sparsity)*(1.-sparsity_list[j])*k*p
+    #positive = (1.-task_sparsity)*(1.-sparsity_list[j])*k*p
     negative = k*p - positive
 
     selective_lengths = []
@@ -78,7 +78,7 @@ for j in range(len(sparsity_list)):
 
     for i in range(len(feature_weight_list)):
         print((i,j),"(i,j)")
-        sims = test_coverage(feature_weight_list[i],[2.5,5.0],p,task_sparsity,sparsity_list[j],nsim=n_list[j])
+        sims = test_coverage(feature_weight_list[i],[2.5,5.0],p,sparsity_list[j],global_sparsity,nsim=n_list[j])
         selective_coverage.append(sims[3])
         selective_coverage2.append(sims[4])
         naive_coverage.append(sims[5])
@@ -301,6 +301,7 @@ ax3.set_title("Accuracy", y = 1.01,fontsize=20)
 def common_format(ax):
     ax.grid(True, which='both',color='#f0f0f0')
     ax.set_xlabel('Task Sparsity', fontsize=18)
+    #ax.set_xlabel('Global Sparsity', fontsize=18)
     return ax
 
 common_format(ax1)
