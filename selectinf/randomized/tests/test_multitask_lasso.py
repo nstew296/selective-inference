fix issue re scaling

selectinf/randomized/tests/test_multitask_lasso.py
@@ -388,9 +388,7 @@ def test_multitask_lasso_data_splitting(predictor_vars_train,
 
     # Compute snesitivity and specificity after inference
     true_active = np.transpose(np.nonzero(np.transpose(beta)))
-    print(true_active,"true active")
     num_positive = np.shape(true_active)[0]
-    print(num_positive,"num_positive")
     if (active_signs != 0).sum() > 0:
         selected_active = np.transpose(np.nonzero(np.transpose(active_signs)))
         true_positive_selected = [x in true_active.tolist() for x in selected_active.tolist()]
@@ -665,7 +663,7 @@ def test_coverage(weight,signal,p,ts,gs,nsim=100):
     single_task_selective_test_error_list2 = []
 
     ntask = 5
-    nsamples= 2000 * np.ones(ntask)
+    nsamples= 1000 * np.ones(ntask)
     p=p
     global_sparsity= gs
     task_sparsity= ts
@@ -688,6 +686,12 @@ def test_coverage(weight,signal,p,ts,gs,nsim=100):
                                                                                            rhos,
                                                                                            random_signs=True,
                                                                                            equicorrelated=True)[:4]
+        SIG = np.full((p,p),0.3)
+        np.fill_diagonal(SIG,1.0)
+        SNR = beta.T.dot(SIG.dot(beta))/1000
+        SNR = np.diag(SNR)
+        print(SNR,"SNR")
+        print(SNR/(1+SNR), "PVE")
 
     if link == "logit":
         response_vars, predictor_vars, beta, gaussian_noise = logistic_multitask_instance(ntask,
@@ -726,9 +730,9 @@ def test_coverage(weight,signal,p,ts,gs,nsim=100):
                 return tdist.rvs(df, size=n) / sd_t
 
             if link == "identity":
-                noise = _noise(nsamples.sum(), np.inf)
+                noise = _noise(nsamples.sum()*2, np.inf)
                 response_vars = {}
-                nsamples_cumsum = np.cumsum(nsamples)
+                nsamples_cumsum = np.cumsum([nsamples[i]*2 for i in range(ntask)])
                 for i in range(ntask):
                     if i == 0:
                         response_vars[i] = (predictor_vars[i].dot(beta[:, i]) + noise[:nsamples_cumsum[i]]) * \
@@ -755,8 +759,8 @@ def test_coverage(weight,signal,p,ts,gs,nsim=100):
 
         print(n,"n sim")
 
-        samples = np.arange(np.int(nsamples[0]))
-        train = np.random.choice(samples, size=np.int(0.5*nsamples[0]), replace=False)
+        samples = np.arange(np.int(nsamples[0]*2))
+        train = np.random.choice(samples, size=np.int(nsamples[0]), replace=False)
         test = np.setdiff1d(samples, train)
 
         response_vars_train = {j: response_vars[j][train] for j in range(ntask)}

selectinf/randomized/tests/vary_task_sparsity.py
@@ -8,12 +8,12 @@ from selectinf.randomized.tests.test_multitask_lasso import test_coverage
 
 k=5
 p=100
-global_sparsity = 0.95
+global_sparsity = 0.90
 #task_sparsity = 0.4
 
 length_path = 15
-lambdamin = 0
-lambdamax = 4.0
+lambdamin = 0.5
+lambdamax = 5.0
 #weights = np.arange(np.log(lambdamin), np.log(lambdamax), (np.log(lambdamax) - np.log(lambdamin)) / (length_path))
 #feature_weight_list = np.exp(weights)
 feature_weight_list = np.arange(lambdamin, lambdamax,(lambdamax - lambdamin) / (length_path))
@@ -24,7 +24,7 @@ df = pd.DataFrame(columns=['Task Sparsity', 'Method', 'Coverage', 'Length'])
 
 sparsity_list = [0.0,0.2,0.4,0.6]
 #sparsity_list = [0.80,0.85,0.9,0.95]
-n_list = [100,100,100,100]
+n_list = [5,5,5,5]
 ##n_list = [5,5,5,20,20]
 coverage_by_ts = {j: [[], [], [], [], [], [], []] for j in range(len(sparsity_list))}
 length_by_ts = {j: [[], [], [], [], [], [], []] for j in range(len(sparsity_list))}
@@ -79,7 +79,7 @@ for j in range(len(sparsity_list)):
     for i in range(len(feature_weight_list)):
         print((i,j),"(i,j)")
         weight = [feature_weight_list[i]]*7
-        sims = test_coverage(weight,[2.5,5.0],p,sparsity_list[j],global_sparsity,nsim=n_list[j])
+        sims = test_coverage(weight,[1.0,3.0],p,sparsity_list[j],global_sparsity,nsim=n_list[j])
         selective_coverage.append(sims[3])
         selective_coverage2.append(sims[4])
         naive_coverage.append(sims[5])

selectinf/tests/instance.py
@@ -391,7 +391,7 @@ def gaussian_multitask_instance(ntask,
                                 equicorrelated=False):
 
     np.random.seed(5)
-    predictor_vars= {i: _design(nsamples[i], p, rhos[i], equicorrelated)[0] for i in range(ntask)}
+    predictor_vars= {i: _design(nsamples[i]*2, p, rhos[i], equicorrelated)[0] for i in range(ntask)}
 
     if center:
         predictor_vars = {i: predictor_vars[i]-predictor_vars[i].mean(0)[None, :] for i in range(ntask)}
@@ -439,9 +439,9 @@ def gaussian_multitask_instance(ntask,
             sd_t = np.std(tdist.rvs(df, size=50000))
         return tdist.rvs(df, size=n) / sd_t
 
-    gaussian_noise = _noise(nsamples.sum() + p*ntask, df)
+    gaussian_noise = _noise(nsamples.sum()*2 + p*ntask, df)
     response_vars = {}
-    nsamples_cumsum = np.cumsum(nsamples)
+    nsamples_cumsum = np.cumsum([nsamples[i]*2 for i in range(ntask)])
     for i in range(ntask):
         if i == 0:
             response_vars[i] = (predictor_vars[i].dot(beta[:, i]) + gaussian_noise[:nsamples_cumsum[i]]) * sigma[i]