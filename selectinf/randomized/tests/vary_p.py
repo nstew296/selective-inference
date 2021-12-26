804cbc562f1784f760e1f92f7beff228ab5664be
nstew <nstew@umich.edu>
2021-12-26 00:39:20 -0600

    plot pivots

--- selectinf/randomized/tests/vary_p.py
+++ selectinf/randomized/tests/vary_p.py
@@ -12,7 +12,7 @@ task_sparsity = 0.2
 #task_sparsity = 0.4
 
 length_path = 8
-lambdamin = 1.25
+lambdamin = 0.75
 lambdamax = 4.5
 feature_weight_list = np.arange(lambdamin, lambdamax,(lambdamax - lambdamin) / (length_path))
 print(feature_weight_list)
