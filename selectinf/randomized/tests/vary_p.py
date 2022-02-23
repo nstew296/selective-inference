f067c717a892486e31d92b85168387618ecbb2b2
nstew <nstew@umich.edu>
2022-02-22 20:48:08 -0500

    update lambda path in vary p simulation

--- selectinf/randomized/tests/vary_p.py
+++ selectinf/randomized/tests/vary_p.py
@@ -16,8 +16,8 @@ task_sparsity = 0.2
 #task_sparsity = 0.4
 
 
-length_path = 10
-lambdamin = 1.25
+length_path = 15
+lambdamin = 1.0
 lambdamax = 4.5
 feature_weight_list = np.arange(lambdamin, lambdamax,(lambdamax - lambdamin) / (length_path))
 print(feature_weight_list)
